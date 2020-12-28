9aa41d1c0a16438c4be4d1aa8f9185d0c52a749b
work <morvl.work@yandex.ru>
2020-12-28 11:53:03 +0200

    Обработка referer

--- code/Modules/Clicks/Http/Controllers/ClickController.php
+++ code/Modules/Clicks/Http/Controllers/ClickController.php
@@ -17,10 +17,14 @@ class ClickController extends Controller
      */
     public function index(ClickRequest $request)
     {
+
+        $referer = \request()->headers->get('referer') ?? '';
+        $parseReferer = parse_url($referer);
+
         $data = [
             'ua' => \request()->userAgent(),
             'ip' => \request()->ip(),
-            'ref' => \request()->headers->get('HTTP_REFERER') ?? '',
+            'ref' => $parseReferer['host'] ?? '',
             'param1' => $request->get('param1'),
             'param2' => $request->get('param2'),
         ];
@@ -31,25 +35,29 @@ class ClickController extends Controller
 
         $hasError = false;
 
-        if (BadDomain::where(['name' => $data['ref']])->first()) {
-            $data['bad_domain'] = 1;
+        if ($badDomain = BadDomain::where(['name' => $data['ref']])->first()) {
             $hasError = true;
         }
 
         if ($clickInDb) {
+            $clickInDb->bad_domain = $badDomain ? 1 : 0;
             $clickInDb->increment('error');
             $clickInDb->save();
             $hasError = true;
         } else {
             $clickInDb = new Click();
+            if ($badDomain) {
+                $data['error'] = 1;
+                $data['bad_domain'] = 1;
+            }
             $clickInDb->fill($data);
             $clickInDb->save();
         }
 
         if ($hasError) {
-            return redirect(route('error', ['idClick' => $clickInDb->id]))->withErrors('Click was processed already!');
+            return redirect(route('error', ['idClick' => $clickInDb->id]));
         } else {
-            return redirect(route('success', ['idClick' => $clickInDb->id]))->with('status', 'Click success!');
+            return redirect(route('success', ['idClick' => $clickInDb->id]));
         }
     }
 
